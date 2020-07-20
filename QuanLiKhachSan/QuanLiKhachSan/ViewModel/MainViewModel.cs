de391339fc02655fb644072b019be5d6a23f61bd
NguyenHoangQuyen <hoangquyen7901@gmail.com>
2020-07-20 14:16:01 +0700

    # Khởi tạo project với mô hình MVVM, sử dụng package Material design

---
+++ QuanLiKhachSan/QuanLiKhachSan/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLiKhachSan.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool isLoaded = false;
+
+        public MainViewModel()
+        {
+            if (isLoaded != true)
+            {
+                MessageBox.Show("hii");
+            }
+
+        }
+    }
+}
