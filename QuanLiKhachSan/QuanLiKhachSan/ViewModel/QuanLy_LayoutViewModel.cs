c92f81d87d208c5ff6e384ee94f8423e1e92f91d
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-08-23 22:09:54 +0700

    Quản lý Viewmodel
    Các viewmodel nhân viên quản lý
    - Các chức năng quản lý nhân viên

---
+++ QuanLiKhachSan/QuanLiKhachSan/ViewModel/QuanLy_LayoutViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLiKhachSan.ViewModel
+{
+    public class QuanLy_LayoutViewModel: BaseViewModel
+    {
+
+        private object _CurrentQuanLyDataContext;
+        public object CurrentQuanLyDataContext
+        {
+            get => _CurrentQuanLyDataContext;
+            set
+            {
+                _CurrentQuanLyDataContext = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _QuanLyTitle;
+        public string QuanLyTitle
+        {
+            get => _QuanLyTitle;
+            set { _QuanLyTitle = value; OnPropertyChanged(); }
+        }
+        #region All Command
+        public ICommand btnQuanLy_NhanVien { get; set; }
+        public ICommand btnQuanLy_Phong { get; set; }
+        public ICommand btnQuanLy_DichVu { get; set; }
+        public ICommand btnQuanLy_TaiKhoan { get; set; }
+        #endregion
+
+        public QuanLy_LayoutViewModel()
+        {
+            object ucNhanVien = new QuanLyNhanVienViewModel();
+            object ucPhong = new QuanLyPhong_LoaiPhongViewModel();
+            object ucDichVu = new QuanLyDichVu_LoaiDVViewModel();
+            object ucTaiKhoan = new QuanLyTaiKhoanViewModel();
+            CurrentQuanLyDataContext = ucNhanVien;
+            QuanLyTitle = "QUẢN LÝ NHÂN VIÊN";
+
+
+            btnQuanLy_NhanVien = new RelayCommand<object>((p) => { return CurrentQuanLyDataContext != ucNhanVien; }, (p) =>
+            {
+                QuanLyTitle = "QUẢN LÝ NHÂN VIÊN";
+                CurrentQuanLyDataContext = ucNhanVien;
+            });
+
+            btnQuanLy_Phong = new RelayCommand<object>((p) => { return CurrentQuanLyDataContext != ucPhong; }, (p) =>
+            {
+                QuanLyTitle = "QUẢN LÝ PHÒNG, LOẠI PHÒNG";
+                CurrentQuanLyDataContext = ucPhong;
+            });
+
+            btnQuanLy_DichVu = new RelayCommand<object>((p) => { return CurrentQuanLyDataContext != ucDichVu; }, (p) =>
+            {
+                QuanLyTitle = "QUẢN LÝ DỊCH VỤ, LOẠI DỊCH VỤ";
+                CurrentQuanLyDataContext = ucDichVu;
+            });
+
+            btnQuanLy_TaiKhoan = new RelayCommand<object>((p) => { return CurrentQuanLyDataContext != ucTaiKhoan; }, (p) =>
+            {
+                QuanLyTitle = "QUẢN LÝ TÀI KHOẢN";
+                CurrentQuanLyDataContext = ucTaiKhoan;
+            });
+
+
+        }
+    }
+}
